c0b75412bc3735649b1e754483a96a1078117c49
Administrator <Administrator@BD-307>
2019-11-07 21:58:32 +0800

    JavaFramWork 20191107 下課前最後一次Commit_T

--- src/day11_16_combine/model/member.java
+++ src/day11_16_combine/model/member.java
@@ -8,6 +8,47 @@ public class member {
 	private String Address;
 	private String Mobile;
 	private String Phone;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public String getMobile() {
+		return Mobile;
+	}
+	public void setMobile(String mobile) {
+		Mobile = mobile;
+	}
+	public String getPhone() {
+		return Phone;
+	}
+	public void setPhone(String phone) {
+		Phone = phone;
+	}
 
-	//
 }
